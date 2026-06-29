81e02b26cb9d693119c15fb15a31b26b2a27ea32
agent-walkllc <288607573+agent-walkllc@users.noreply.github.com>
2026-06-29 08:34:37 +0000

    fix: pin provider ~>8.20, fix security list ingress, fix budget, update lock

--- terraform/budget/main.tf
+++ terraform/budget/main.tf
@@ -1,14 +1,15 @@
-# resource "oci_budget_budget" "cost_tripwire_budget" {
-#   display_name   = "Free"
-#   amount         = 1
-#   reset_period   = "MONTHLY"
-#   compartment_id = var.tenancy_ocid
-#   target_type    = "COMPARTMENT"
-#   targets        = [var.tenancy_ocid]
-# }
+resource "oci_budget_budget" "dollar_budget" {
+  display_name   = "Free"
+  amount         = 1
+  reset_period   = "MONTHLY"
+  compartment_id = var.tenancy_ocid
+  target_type    = "COMPARTMENT"
+  targets        = [var.tenancy_ocid]
+}
 
 resource "oci_budget_alert_rule" "forecast" {
-  budget_id      = oci_budget_budget.cost_tripwire_budget.id
+  depends_on     = [oci_budget_budget.dollar_budget]
+  budget_id      = oci_budget_budget.dollar_budget.id
   type           = "FORECAST"
   threshold      = 1
   threshold_type = "PERCENTAGE"
@@ -16,7 +17,8 @@ resource "oci_budget_alert_rule" "forecast" {
 }
 
 resource "oci_budget_alert_rule" "actual" {
-  budget_id      = oci_budget_budget.cost_tripwire_budget.id
+  depends_on     = [oci_budget_budget.dollar_budget]
+  budget_id      = oci_budget_budget.dollar_budget.id
   type           = "ACTUAL"
   threshold      = 1
   threshold_type = "PERCENTAGE"
