017f1783408fafaa4238aa998d8d82e148adf921
Rizky <167944030+rzkw@users.noreply.github.com>
2026-08-04 12:43:15 +0700

    renamed budget, deleted existing budgets before redeploying via terraform plan -out/apply

--- terraform/budget/main.tf
+++ terraform/budget/main.tf
@@ -1,5 +1,7 @@
+# Reference: https://github.com/calvinbui/infra/blob/master/oracle-cloud/budget/oci-budget.tf; https://calvin.me/kubernetes-on-oracle-cloud-free-tier/#budget-alerts
+
 resource "oci_budget_budget" "dollar_budget" {
-  display_name   = "Free"
+  display_name   = "Dollar-Budget"
   amount         = 1
   reset_period   = "MONTHLY"
   compartment_id = var.tenancy_ocid
