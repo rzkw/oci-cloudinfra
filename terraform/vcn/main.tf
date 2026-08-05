6da7e94967b23e323db73cbb7ad1a0caf3caf45d
Rizky <167944030+rzkw@users.noreply.github.com>
2026-08-05 11:50:18 +0700

    added vcn/providers.tf; moved backend block from main.tf into providers.tf to be consistent with other modules. edit to PR # 60 by @agent-walkllc

--- terraform/vcn/main.tf
+++ terraform/vcn/main.tf
@@ -1,24 +1,3 @@
-terraform {
-  backend "oci" {
-    bucket    = "tfstate"
-    namespace = "axvczntoncvg"
-    key       = "terraform/vcn/terraform.tfstate"
-    region    = "ap-melbourne-1"
-  }
-
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 8.20"
-    }
-  }
-}
-
-provider "oci" {
-  region              = var.region
-  config_file_profile = "DEFAULT"
-}
-
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
