08ba276f9c45074e0930cab24002dfab679de349
walkable-llc <167944030+rzkw@users.noreply.github.com>
2026-09-04 08:55:43 +0700

    bootstrap (#90)

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -4,4 +4,5 @@ pipx install -y ansible-core
 # Clone your Ansible repository
 git clone https://github.com/rzkw/ansible /home/ubuntu/ansible
 # Run your initial Ansible playbook
+ansible-galaxy collection install -r /home/ubuntu/ansible/collections/requirements.yml
 ansible-playbook /home/ubuntu/ansible/playbooks/server.yml
