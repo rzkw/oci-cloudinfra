8ff422de2677d84c4bbc7328c294e7719d00225d
Rizky <167944030+rzkw@users.noreply.github.com>
2026-09-04 16:04:23 +0700

    changed bootstrap.sh to install ansible-core via apt

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-apt install -y ansible-core
+# Install Ansible
+sudo apt-get install -y ansible-core
+# Clone your Ansible repository
+git clone https://github.com/rzkw/ansible /home/ubuntu/ansible
 # Run your initial Ansible playbook
 ansible-galaxy collection install -r /home/ubuntu/ansible/collections/requirements.yml
 ansible-playbook /home/ubuntu/ansible/playbooks/server.yml
