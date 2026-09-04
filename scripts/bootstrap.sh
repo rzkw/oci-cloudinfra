50878a3caf30c631dd7208887bd9f9f9788f53a4
Rizky <167944030+rzkw@users.noreply.github.com>
2026-09-04 15:40:18 +0700

    added ansible-core to bootstarp instead of user-data.yaml

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install -y ansible-core
 # Run your initial Ansible playbook
 ansible-galaxy collection install -r /home/ubuntu/ansible/collections/requirements.yml
 ansible-playbook /home/ubuntu/ansible/playbooks/server.yml
--- terraform/instances/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ocid" {
-  description = "OCID of the created instance."
-  value       = oci_core_instance.this.id
-}
-
-output "instance_private_ip" {
-  description = "Private IP address of the created instance."
-  value       = oci_core_instance.this.private_ip
-}
